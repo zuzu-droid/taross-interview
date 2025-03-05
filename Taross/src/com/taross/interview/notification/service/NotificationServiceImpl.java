325c42264270629ef23d4259634be243c10aa00e
zuzu-droid <norzulaikah@gmail.com>
2025-03-05 23:19:19 +0800

    Create NotificationServiceImpl.java

---
+++ Taross/src/com/taross/interview/notification/service/NotificationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.taross.interview.notification.service;
+
+public class NotificationServiceImpl implements MessageService{
+
+    @Override
+    public void sendMessage(String msg, String rec) {
+        // TODO Auto-generated method stub
+        System.out.print("Notification sent to [" + rec + "] with Message [" + msg + "]");
+    }
+
+
+}
